f203e71c9027d806b27c4c5d60bb8359ec5dd198
cc <home@iwenli.org>
2018-11-04 16:54:43 +0800

    zhuangpei

--- src/Spider/App.cs
+++ src/Spider/App.cs
@@ -1,6 +1,9 @@
 ﻿using Spider.Demo;
+using Spider.Repository;
 using Spider.Sample;
+using SqlSugar;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Spider
@@ -24,11 +27,30 @@ namespace Spider
 
 			//Task.Factory.StartNew(() => { });
 
-			new MySpider().Run();
+			//new MySpider().Run();
 
+			var _client = new BaseRepository<JMEntity>();
+			var _context = _client.Context;
+			//建表
+			//_context.CreateTableByEntity(false, typeof(QCCEntity));
+
+			var _listSugar = _client.Db.Context.Queryable<JMEntity>()
+				.AS("spider_zsjm_2018_11_03")
+				.Where(m => SqlFunc.Subqueryable<QCCEntity>().Where(s => s.Name == m.Company).NotAny())
+				.GroupBy(m => m.Company).Select(m => m.Company);
+			var _sql = _listSugar.ToSql();
+			Console.WriteLine(_sql);
+			var _list = _listSugar.ToList();
+
+			var _qccSpider = new QCCSpider();
 
 			//MyDownloader.GetHtml();
-			//new QCCSpider().Run();
+			foreach (var item in _list)
+			{
+				_qccSpider.AddRequestByKey(item);
+			}
+
+			_qccSpider.Run();
 
 			Console.WriteLine("按任意键退出...");
             Console.Read();
--- src/Spider/QCCSpider.cs
+++ src/Spider/QCCSpider.cs
@@ -10,6 +10,8 @@ using DotnetSpider.Extraction.Model;
 using DotnetSpider.Extraction.Model.Attribute;
 using Newtonsoft.Json;
 using Spider.Demo;
+using Spider.Repository;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,34 +25,36 @@ namespace Spider
 	{
 		List<string> List = new
 			List<string>
-		{   "上海速时网络科技有限公司"
-			,"武汉振兴鑫诚装饰材料有限公司"
-			,"广州德膳企业管理有限公司"
-			,"佛山市威能建材有限公司	"
-			,"湖北澳克玛农业有限公司	"
-			,"秦皇岛合纵力餐饮管理有限公司"
-			,"湖北一池天下生态农业科技有限公司"
-			,"江苏欧象万盛木业有限公司"
-			,"湖南芯瑞通光电科技有限公司"
-			,"北京汉釜宫国际餐饮管理有限公司	"
-			,"昆明闺蜜内衣有限公司	"
-			,"中创时代(北京)国际投资管理有限公司	"
-			,"北京瑞科同创能源科技有限公司	"
-			,"惠州市中天阳光电力光伏有限公司	"
-			,"秦皇岛合纵力餐饮管理有限公司	"
-			,"昆明西汇农业科技有限公司	"
-			,"山东普达企业管理咨询有限公司	"
-			,"江苏盐城宏凯鹅苗孵化中心	"
-			,"重庆泉佳喜餐饮文化有限公司	"
-			,"南京九州盛世餐饮管理有限公司	"
-			,"湖北妙农农产品有限公司	"
-			,"秦皇岛合纵力科技开发有限公司	"
-			,"昆明西汇农业科技有限公司	"
-			,"广州德膳企业管理有限公司	"
-			,"济南瑞粮餐饮管理有限公司	"
-			,"上海郁隆禾餐饮管理有限公司	"
-			,"浙江食叁味餐饮管理有限公司	"
-			,"上海速时网络科技有限公司	"};
+		{
+			//"上海速时网络科技有限公司"
+			//,"武汉振兴鑫诚装饰材料有限公司"
+			//,"广州德膳企业管理有限公司"
+			//,"佛山市威能建材有限公司	"
+			//,"湖北澳克玛农业有限公司	"
+			//,"秦皇岛合纵力餐饮管理有限公司"
+			//,"湖北一池天下生态农业科技有限公司"
+			//,"江苏欧象万盛木业有限公司"
+			//,"湖南芯瑞通光电科技有限公司"
+			//,"北京汉釜宫国际餐饮管理有限公司	"
+			//,"昆明闺蜜内衣有限公司	"
+			//,"中创时代(北京)国际投资管理有限公司	"
+			//,"北京瑞科同创能源科技有限公司	"
+			//,"惠州市中天阳光电力光伏有限公司	"
+			//,"秦皇岛合纵力餐饮管理有限公司	"
+			//,"昆明西汇农业科技有限公司	"
+			//,"山东普达企业管理咨询有限公司	"
+			//,"江苏盐城宏凯鹅苗孵化中心	"
+			//,"重庆泉佳喜餐饮文化有限公司	"
+			//,"南京九州盛世餐饮管理有限公司	"
+			//,"湖北妙农农产品有限公司	"
+			//,"秦皇岛合纵力科技开发有限公司	"
+			//,"昆明西汇农业科技有限公司	"
+			//,"广州德膳企业管理有限公司	"
+			//,"济南瑞粮餐饮管理有限公司	"
+			//,"上海郁隆禾餐饮管理有限公司	"
+			//,"浙江食叁味餐饮管理有限公司	"
+			//,"上海速时网络科技有限公司	"
+		};
 
 		protected override void OnInit(params string[] arguments)
 		{
@@ -71,6 +75,14 @@ namespace Spider
 			AddEntityType<QCCEntity>();
 		}
 
+		public void AddRequestByKey(string key)
+		{
+			if (key != null && key.Length > 2)
+			{
+				AddRequests("https://www.qichacha.com/search?key=" + key.Trim());
+			}
+		}
+
 		private class MyProcessor : BasePageProcessor
 		{
 			public MyProcessor()
@@ -89,34 +101,38 @@ namespace Spider
 
 				if (page.TargetUrl.Contains("firm_"))
 				{
+					var _el = page.Selectable();
+					var _el1 = _el.Select(Selectors.XPath(".//div[@id='company-top']/div[1]/div[2]"));
+					var _el2 = _el.Select(Selectors.XPath(".//section[@id='Cominfo']/table[2]"));
 					page.AddResultItem("QCCEntity", new QCCEntity
 					{
-						Name = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[1]/h1"))?.GetValue()?.Trim() ?? "",
-						Mobile = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[3]/span[1]/span[2]/span"))?.GetValue()?.Trim() ?? "",
-						WebSite = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[3]/span[3]/a[1]/@href"))?.GetValue()?.Trim() ?? "",
-						Email = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[4]/span[1]/span[2]/a[1]"))?.GetValue()?.Trim() ?? "",
-						Address = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[4]/span[3]/a[1]"))?.GetValue()?.Trim() ?? "",
-						Introduction = page.Selectable().Select(Selectors.XPath("//div[@id='company-top']/div[1]/div[2]/div[5]/span[2]"))?.GetValue()?.Trim() ?? "",
+						Name = _el1.Select(Selectors.XPath(".//div[1]/h1"))?.GetValue()?.Trim() ?? "",
+						State = _el1.Select(Selectors.XPath(".//div[2]/span"))?.GetValue()?.Trim() ?? "",
+						Mobile = _el1.Select(Selectors.XPath(".//div[3]/span[1]/span[2]/span"))?.GetValue()?.Trim() ?? "",
+						WebSite = _el1.Select(Selectors.XPath(".//div[3]/span[3]/a[1]/@href"))?.GetValue()?.Trim() ?? "",
+						Email = _el1.Select(Selectors.XPath(".//div[4]/span[1]/span[2]/a[1]"))?.GetValue()?.Trim() ?? "",
+						Address = _el1.Select(Selectors.XPath(".//div[4]/span[3]/a[1]"))?.GetValue()?.Trim() ?? "",
+						Introduction = _el1.Select(Selectors.XPath(".//div[5]/span[2]"))?.GetValue()?.Trim() ?? "",
 
-						RegisteredCapital = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[1]/td[2]"))?.GetValue()?.Trim() ?? "",
-						PaidInCapital = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[1]/td[4]"))?.GetValue()?.Trim() ?? "",
-						State = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[2]/td[2]"))?.GetValue()?.Trim() ?? "",
-						FoundDate = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[2]/td[4]"))?.GetValue()?.Trim() ?? "",
-						USCD = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[3]/td[2]"))?.GetValue()?.Trim() ?? "",
-						TIN = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[3]/td[4]"))?.GetValue()?.Trim() ?? "",
-						RN = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[4]/td[2]"))?.GetValue()?.Trim() ?? "",
-						OC = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[4]/td[4]"))?.GetValue()?.Trim() ?? "",
-						Type = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[5]/td[2]"))?.GetValue()?.Trim() ?? "",
-						Industry = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[5]/td[4]"))?.GetValue()?.Trim() ?? "",
-						ApprovalDate = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[6]/td[2]"))?.GetValue()?.Trim() ?? "",
-						RA = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[6]/td[4]"))?.GetValue()?.Trim() ?? "",
-						Area = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[7]/td[2]"))?.GetValue()?.Trim() ?? "",
-						EName = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[7]/td[4]"))?.GetValue()?.Trim() ?? "",
-						OName = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[8]/td[2]"))?.GetValue()?.Trim() ?? "",
-						NOP = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[8]/td[4]"))?.GetValue()?.Trim() ?? "",
-						StaffSize = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[9]/td[2]"))?.GetValue()?.Trim() ?? "",
-						OperatingPeriod = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[9]/td[4]"))?.GetValue()?.Trim() ?? "",
-						BusinessScope = page.Selectable().Select(Selectors.XPath("//*[@id='Cominfo']/table[2]/tbody/tr[11]/td[2]"))?.GetValue()?.Trim() ?? "",
+						RegisteredCapital = _el2.Select(Selectors.XPath(".//tr[1]/td[2]"))?.GetValue()?.Trim() ?? "",
+						PaidInCapital = _el2.Select(Selectors.XPath(".//tr[1]/td[4]"))?.GetValue()?.Trim() ?? "",
+						StateDetails = _el2.Select(Selectors.XPath(".//tr[2]/td[2]"))?.GetValue()?.Trim() ?? "",
+						FoundDate = _el2.Select(Selectors.XPath(".//tr[2]/td[4]"))?.GetValue()?.Trim() ?? "",
+						USCD = _el2.Select(Selectors.XPath(".//tr[3]/td[2]"))?.GetValue()?.Trim() ?? "",
+						TIN = _el2.Select(Selectors.XPath(".//tr[3]/td[4]"))?.GetValue()?.Trim() ?? "",
+						RN = _el2.Select(Selectors.XPath(".//tr[4]/td[2]"))?.GetValue()?.Trim() ?? "",
+						OC = _el2.Select(Selectors.XPath(".//tr[4]/td[4]"))?.GetValue()?.Trim() ?? "",
+						Type = _el2.Select(Selectors.XPath(".//tr[5]/td[2]"))?.GetValue()?.Trim() ?? "",
+						Industry = _el2.Select(Selectors.XPath(".//tr[5]/td[4]"))?.GetValue()?.Trim() ?? "",
+						ApprovalDate = _el2.Select(Selectors.XPath(".//tr[6]/td[2]"))?.GetValue()?.Trim() ?? "",
+						RA = _el2.Select(Selectors.XPath(".//tr[6]/td[4]"))?.GetValue()?.Trim() ?? "",
+						Area = _el2.Select(Selectors.XPath(".//tr[7]/td[2]"))?.GetValue()?.Trim() ?? "",
+						EName = _el2.Select(Selectors.XPath(".//tr[7]/td[4]"))?.GetValue()?.Trim() ?? "",
+						OName = _el2.Select(Selectors.XPath(".//tr[8]/td[2]/span"))?.GetValue()?.Trim() ?? "",
+						NOP = _el2.Select(Selectors.XPath(".//tr[8]/td[4]"))?.GetValue()?.Trim() ?? "",
+						StaffSize = _el2.Select(Selectors.XPath(".//tr[9]/td[2]"))?.GetValue()?.Trim() ?? "",
+						OperatingPeriod = _el2.Select(Selectors.XPath(".//tr[9]/td[4]"))?.GetValue()?.Trim() ?? "",
+						BusinessScope = _el2.Select(Selectors.XPath(".//tr[11]/td[2]"))?.GetValue()?.Trim() ?? "",
 						Uri = page.TargetUrl
 					});
 				}
@@ -124,6 +140,7 @@ namespace Spider
 		}
 		private class MyPipeline : BasePipeline
 		{
+			BaseRepository<QCCEntity> Repository = new BaseRepository<QCCEntity>();
 			public override void Process(IList<ResultItems> resultItems, dynamic sender = null)
 			{
 
@@ -131,119 +148,168 @@ namespace Spider
 				{
 					if (resultItem["QCCEntity"] != null)
 					{
-						Console.WriteLine($"抽取数据: {JsonConvert.SerializeObject(resultItem["QCCEntity"])}");
+						var _entity = resultItem["QCCEntity"] as QCCEntity;
+						try
+						{
+							var result = Repository.Add(_entity).Result;
+							Console.WriteLine($"抽取数据[{result}]: {JsonConvert.SerializeObject(_entity)}");
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine($"抽取数据异常:{JsonConvert.SerializeObject(_entity)}");
+							Console.WriteLine(ex.ToString());
+						}
+
 					}
 				}
 				// 可以自由实现插入数据库或保存到文件
 			}
 		}
+	}
 
-		class QCCEntity : IBaseEntity
-		{
-			/// <summary>
-			/// 企业名称
-			/// </summary>
-			public string Name { set; get; }
-			/// <summary>
-			/// 企业官网
-			/// </summary>
-			public string WebSite { set; get; }
-			/// <summary>
-			/// 企业电话
-			/// </summary>
-			public string Mobile { set; get; }
-			/// <summary>
-			/// 企业邮箱
-			/// </summary>
-			public string Email { set; get; }
-			/// <summary>
-			/// 企业地址
-			/// </summary>
-			public string Address { set; get; }
-			/// <summary>
-			/// 企业简介
-			/// </summary>
-			public string Introduction { set; get; }
-			/// <summary>
-			/// 注册资本
-			/// </summary>
-			public string RegisteredCapital { set; get; }
-			/// <summary>
-			/// 实缴资本
-			/// </summary>
-			public string PaidInCapital { set; get; }
-			/// <summary>
-			/// 经营状态
-			/// </summary>
-			public string State { set; get; }
-			/// <summary>
-			/// 成立日期
-			/// </summary>
-			public string FoundDate { set; get; }
-			/// <summary>
-			/// 统一社会信用代码
-			/// </summary>
-			public string USCD { set; get; }
-			/// <summary>
-			/// 纳税人识别号
-			/// </summary>
-			public string TIN { set; get; }
-			/// <summary>
-			/// 注册号
-			/// </summary>
-			public string RN { set; get; }
-			/// <summary>
-			/// 组织机构代码
-			/// </summary>
-			public string OC { set; get; }
-			/// <summary>
-			/// 公司类型
-			/// </summary>
-			public string Type { set; get; }
-			/// <summary>
-			/// 行业
-			/// </summary>
-			public string Industry { set; get; }
-			/// <summary>
-			/// 核准日期
-			/// </summary>
-			public string ApprovalDate { set; get; }
-			/// <summary>
-			/// 登记机关
-			/// </summary>
-			public string RA { set; get; }
-			/// <summary>
-			/// 所属地区
-			/// </summary>
-			public string Area { set; get; }
-			/// <summary>
-			/// 英文名
-			/// </summary>
-			public string EName { set; get; }
-			/// <summary>
-			/// 曾用名
-			/// </summary>
-			public string OName { set; get; }
-			/// <summary>
-			/// 参保人数
-			/// </summary>
-			public string NOP { set; get; }
-			/// <summary>
-			/// 人员规模
-			/// </summary>
-			public string StaffSize { set; get; }
-			/// <summary>
-			/// 营业期限
-			/// </summary>
-			public string OperatingPeriod { set; get; }
-			/// <summary>
-			/// 经营范围
-			/// </summary>
-			public string BusinessScope { set; get; }
-			/// <summary>
-			/// 企查查网址
-			/// </summary>
-			public string Uri { set; get; }
-		}
+
+	class QCCEntity : IBaseEntity
+	{
+		/// <summary>
+		///	自增
+		/// </summary>
+		[SugarColumn(IsNullable = false)]
+		public long Id { set; get; }
+
+		/// <summary>
+		/// 企业名称
+		/// </summary>
+		[SugarColumn(IsNullable = false, IsPrimaryKey = true, Length = 100)]
+		public string Name { set; get; }
+		/// <summary>
+		/// 企业官网
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string WebSite { set; get; }
+		/// <summary>
+		/// 企业电话
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string Mobile { set; get; }
+		/// <summary>
+		/// 企业邮箱
+		/// </summary> 
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string Email { set; get; }
+		/// <summary>
+		/// 企业地址
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 500)]
+		public string Address { set; get; }
+		/// <summary>
+		/// 企业简介
+		/// </summary> 
+		[SugarColumn(IsNullable = true, Length = 1000)]
+		public string Introduction { set; get; }
+		/// <summary>
+		/// 注册资本
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string RegisteredCapital { set; get; }
+		/// <summary>
+		/// 实缴资本
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string PaidInCapital { set; get; }
+		/// <summary>
+		/// 经营状态
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 10)]
+		public string State { set; get; }
+		/// <summary>
+		/// 经营状态详情
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string StateDetails { set; get; }
+		/// <summary>
+		/// 成立日期
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string FoundDate { set; get; }
+		/// <summary>
+		/// 统一社会信用代码
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string USCD { set; get; }
+		/// <summary>
+		/// 纳税人识别号
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string TIN { set; get; }
+		/// <summary>
+		/// 注册号
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string RN { set; get; }
+		/// <summary>
+		/// 组织机构代码
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string OC { set; get; }
+		/// <summary>
+		/// 公司类型
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string Type { set; get; }
+		/// <summary>
+		/// 行业
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string Industry { set; get; }
+		/// <summary>
+		/// 核准日期
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string ApprovalDate { set; get; }
+		/// <summary>
+		/// 登记机关
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string RA { set; get; }
+		/// <summary>
+		/// 所属地区
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string Area { set; get; }
+		/// <summary>
+		/// 英文名
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 500)]
+		public string EName { set; get; }
+		/// <summary>
+		/// 曾用名
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string OName { set; get; }
+		/// <summary>
+		/// 参保人数
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string NOP { set; get; }
+		/// <summary>
+		/// 人员规模
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string StaffSize { set; get; }
+		/// <summary>
+		/// 营业期限
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 50)]
+		public string OperatingPeriod { set; get; }
+		/// <summary>
+		/// 经营范围
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 5000)]
+		public string BusinessScope { set; get; }
+		/// <summary>
+		/// 企查查网址
+		/// </summary>
+		[SugarColumn(IsNullable = true, Length = 100)]
+		public string Uri { set; get; }
 	}
 }
