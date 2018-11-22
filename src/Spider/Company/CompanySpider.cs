d511cbe91de711afcc0256f264a527f97bfae067
tx.cc[zyl] <admin@iwenli.org>
2018-11-22 13:15:56 +0800

    1

--- src/Spider/Company/CompanySpider.cs
+++ src/Spider/Company/CompanySpider.cs
@@ -23,7 +23,7 @@ namespace Spider.Company
 			InitData();
 
 			Identity = ("CompanySpider_" + DateTime.Now.ToString("yyyy_MM_dd_HHmmss"));
-			Downloader.AddCookie(new System.Net.Cookie("QCCSESSID", "tc8qnunpciiofa7ll1bgv7ts67", "/", ".qichacha.com"));
+			Downloader.AddCookie(new System.Net.Cookie("QCCSESSID", "u1qlm3camss8fo0cg61ltd6010", "/", ".qichacha.com"));
 			SleepTime = 1000 * 5;
 			EmptySleepTime = 1000 * 60 * 10;
 
@@ -33,19 +33,33 @@ namespace Spider.Company
 			AddRequests("https://www.qichacha.com/search?key=石头剪刀布地址电饭锅电饭锅电饭锅电饭锅多个");
 			AddEntityType<CompanyEntity>();
 		}
-		 
+
 		void InitData()
 		{
 			var _client = new BaseRepository<JMEntity>();
 			var _context = _client.Context;
 
-			var _listSugar = _client.Db.Context.Queryable<JMEntity>()
-				.AS("spider_obj")
-				.Where(m => m.Company != "")
-				.Where(m => SqlFunc.Subqueryable<CompanyEntity>().Where(s => s.Name == m.Company).NotAny())
-				.GroupBy(m => m.Company).OrderBy(m => m.Company, OrderByType.Desc).Select(m => m.Company.Trim());
+			var _sql = @"SELECT  [company]
+							FROM
+							(
+								SELECT [company],MAX(creation_time) AS creation_time
+								FROM spider_obj
+								WHERE NOT EXISTS
+								(
+									SELECT 1 FROM dbo.company WHERE name = company
+								)
+									  AND LEN(company) > 2
+								GROUP BY [company]
+							) A ORDER BY creation_time DESC";
+			var _listSugar = _client.Db.Ado.SqlQuery<string>(_sql);
 
-			var _list = _listSugar.ToList();
+			//.Context.Queryable<JMEntity>()
+			//.AS("spider_obj")
+			//.Where(m => m.Company != "")
+			//.Where(m => SqlFunc.Subqueryable<CompanyEntity>().Where(s => s.Name == m.Company).NotAny())
+			//.GroupBy(m => m.Company).Select(m => new {name =  m.Company.Trim() });
+
+			var _list = _listSugar.ToList();//.Skip(1000);
 			CompanyNameQueue = new ConcurrentQueue<string>(_list.Select(m => m.Trim()));
 
 			Logger.Log(LogLevel.Information, 1, this, null,
